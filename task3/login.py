d05f49e7d67db659900b34df08113a2bf8f4c94a
kaushal320 <127113014+kaushal320@users.noreply.github.com>
2024-01-16 10:38:45 +0545

    Delete task3 directory

--- task3/login.py
+++
@@ -1,8 +0,0 @@
-def login_user(username, password):
-    with open('passwd.txt', 'r') as f:
-        for line in f:
-            u, _, p = line.strip().split(':')
-            if u == username and p == password:
-                print("Access granted.")
-                return
-    print("Access denied.")
